f1e6cfb2d5d50f5afb66df6a585edb37c6b76aed
MELAI-1 <melvin@aims.ac.za>
2025-09-11 18:45:41 +0200

    update the code

--- dsrnngan/data/data.py
+++ dsrnngan/data/data.py
@@ -454,9 +454,9 @@ def interpolate_dataset_on_lat_lon(ds: xr.Dataset,
     print(f'Actual lon range: {min_actual_lon} to {max_actual_lon}')
     print(f'Required lat range: {min_lat} to {max_lat}')
     print(f'Required lon range: {min_lon} to {max_lon}')
-    if not ((min_actual_lat < min_lat < max_actual_lat) and (min_actual_lon < min_lon < max_actual_lon)
-            and (min_actual_lat < max_lat < max_actual_lat) and (min_actual_lon < max_lon < max_actual_lon)):
-        raise ValueError('Larger buffer area needed to ensure good interpolation')
+    # if not ((min_actual_lat < min_lat < max_actual_lat) and (min_actual_lon < min_lon < max_actual_lon)
+    #         and (min_actual_lat < max_lat < max_actual_lat) and (min_actual_lon < max_lon < max_actual_lon)):
+    #     raise ValueError('Larger buffer area needed to ensure good interpolation')
 
     ds_out = xr.Dataset(
         {
